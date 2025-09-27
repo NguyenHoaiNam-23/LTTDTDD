86b528dfbf5c72a19e7f20dad3839a1556d6867c
hoai nam_23 <23115053122123@sv.ute.udn.vn>
2025-09-27 20:05:41 +0700

    Day BTVN lan 3

--- BTVN2/app/src/main/java/com/example/btvn2/SecondActivity.java
+++ BTVN2/app/src/main/java/com/example/btvn2/SecondActivity.java
@@ -1,4 +1,26 @@
 package com.example.btvn2;
 
-public class SecondActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Button btnBack = findViewById(R.id.btnback);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
 }
